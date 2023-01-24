a94b21fcc83ff0ec7389201c15bd1b5a2980e3bf
이병훈 <qudgns1081@gmail.com>
2023-01-24 23:15:31 +0900

    # 메인 페이지 서버 연결

--- app/src/main/java/com/hannip/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hannip/myapplication/ui/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.hannip.myapplication.ui.home
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +12,17 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hannip.myapplication.GetWriterList
+import com.hannip.myapplication.MyApp
 import com.hannip.myapplication.databinding.FragmentHomeBinding
+import com.hannip.myapplication.ui.adapter.WriterListAdapter
 import com.hannip.myapplication.ui.module.MainFunction
 import com.hannip.myapplication.ui.module.WriterData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class HomeFragment : Fragment()  {
@@ -39,15 +48,11 @@ class HomeFragment : Fragment()  {
         val contentT: TextView = binding.content
         val likeB : Button = binding.likeB
         val dislikeB : Button = binding.dislikeB
-        val writer1: TextView = binding.gsWriterNickName1
-        val intro1: TextView = binding.gsWriterIntro1
-        val follower1 : TextView = binding.gsWriterFollower1
-        val writer2: TextView = binding.gsWriterNickName2
-        val intro2: TextView = binding.gsWriterIntro2
-        val follower2 : TextView = binding.gsWriterFollower2
-        val writer3: TextView = binding.gsWriterNickName3
-        val intro3: TextView = binding.gsWriterIntro3
-        val follower3 : TextView = binding.gsWriterFollower3
+        val writerTop3List :RecyclerView = binding.writerTop3List
+        val lm = LinearLayoutManager(context)
+        writerTop3List.layoutManager = lm
+//        writerTop3List.setHasFixedSize(true)
+
         homeViewModel.text.observe(viewLifecycleOwner) {
             // 버튼을 누르면 다음 글을 보여줌
             contentT.text = getContentValue()
@@ -57,16 +62,8 @@ class HomeFragment : Fragment()  {
             dislikeB.setOnClickListener {
                 contentT.text = getContentValue()
             }
-            val list = getWriterData()
-            writer1.text = list[0].writer
-            writer2.text = list[1].writer
-            writer3.text = list[2].writer
-            intro1.text = list[0].intro
-            intro2.text = list[1].intro
-            intro3.text = list[2].intro
-            follower1.text = list[0].Like.toString()
-            follower2.text = list[1].Like.toString()
-            follower3.text = list[2].Like.toString()
+            getWriterData()
+
         }
         return root
     }
@@ -74,12 +71,31 @@ class HomeFragment : Fragment()  {
         i++
         return MainFunction().getContent1Data(i).Content+"\n" + MainFunction().getContent1Data(i).writer
     }
-    private fun getWriterData () : List<WriterData>{
-        return MainFunction().getWriterData()
-    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
+    private val getWriterListApi = MyApp().retrofit.create(GetWriterList::class.java)
+
+    // writer top3 데이터 가져오기
+    private fun getWriterData() {
+        val writerTop3List :RecyclerView = binding.writerTop3List
+        val arrayList = arrayListOf<WriterData>()
+        getWriterListApi.postRequest().enqueue(object:
+            Callback<ArrayList<WriterData>> {
+            override fun onResponse(call: Call<ArrayList<WriterData>>, response: Response<ArrayList<WriterData>>) {
+                for (list in response.body()!!){
+                    arrayList.add(list)
+                    Log.i("tag1", arrayList[0].userId)
+                }
+                writerTop3List.adapter = WriterListAdapter(requireContext(), arrayList)
+            }
+
+            override fun onFailure(call: Call<ArrayList<WriterData>>, t: Throwable) {
+                Log.d("response : ", t.toString())
+            }
+        })
+    }
 }
--- app/src/main/java/com/hannip/myapplication/ui/module/ContentData.kt
+++ app/src/main/java/com/hannip/myapplication/ui/module/ContentData.kt
@@ -3,4 +3,4 @@ package com.hannip.myapplication.ui.module
 import java.util.*
 
 data class ContentData(val title:String, val Content:String, val Like:Long, val writer:String)
-data class WriterData(val writer:String, val intro:String, val Like:Long)
+data class WriterData(val userId:String, val userNickname:String, val follower:Long)
